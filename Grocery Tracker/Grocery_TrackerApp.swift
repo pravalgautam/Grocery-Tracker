d5769a1944fb3dfe162b57eba528de75730ca674
Praval Gautam <pravalgautam79@gmail.com>
2024-08-01 21:22:40 +0530

    Initial Commit

---
+++ Grocery Tracker/Grocery_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_TrackerApp.swift
+//  Grocery Tracker
+//
+//  Created by Praval Gautam on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
